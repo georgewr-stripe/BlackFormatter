9f2425b3a259cf50ce630d30fb159ec05c7f5d3c
George Rowberry <95346456+georgewr-stripe@users.noreply.github.com>
2022-03-30 20:08:31 +0000

    update black method

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, Response, request
-from black import format_str, FileMode
+from black import format_file_contents, FileMode
 
 app = Flask(__name__)
 
@@ -11,8 +11,9 @@ def format_response(body, code=200):
 
 @app.route('/format', methods=['POST'])
 def format():
-  data = request.get_json()
-  code = data.get('code', None)
-  if (code):
-      return format_response(format_str(code, mode=FileMode()))
-  return format_response({'error': 'POST param code missing'})
+    data = request.get_json()
+    code = data.get('code', None)
+    if (code):
+        return format_response(
+            format_file_contents(code, mode=FileMode(), fast=False))
+    return format_response({'error': 'POST param code missing'})
